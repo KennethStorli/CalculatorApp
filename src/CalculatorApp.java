0879c9f6816664926a1d571bf1f0925ab8c59ca6
Kenneth Storli <kenneth.storli@gmail.com>
2018-09-12 11:42:31 +0200

    Initial commit

---
+++ src/CalculatorApp.java
@@ -0,0 +1,265 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorApp extends JFrame{
+
+    CalculatorApp app = this;
+    JLabel displayLabel;
+
+    String firstArg = "";
+    String secondArg = "";
+    String operator = "";
+
+
+    public CalculatorApp() {
+        setTitle("Calculator");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new GridLayout(5,5));
+        //Lag 2 jpanel, en med fluid en med grid
+
+        displayLabel = new JLabel("Test");
+        displayLabel.setBounds(0,0,500,100);
+        this.add(displayLabel);
+
+        JButton enterButton = new JButton("Enter");
+        enterButton.setBounds(400,100,100,50);
+        this.add(enterButton);
+        enterButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                enterButton();
+            }
+        });
+
+        JButton zeroButton = new JButton("0");
+        zeroButton.setBounds(400,100,100,50);
+        this.add(zeroButton);
+        zeroButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "0";
+                numberButton(num);
+            }
+        });
+
+        JButton oneButton = new JButton("1");
+        oneButton.setBounds(400,100,100,50);
+        this.add(oneButton);
+        oneButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "1";
+                numberButton(num);
+            }
+        });
+
+        JButton twoButton = new JButton("2");
+        twoButton.setBounds(400,100,100,50);
+        this.add(twoButton);
+        twoButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "2";
+                numberButton(num);
+            }
+        });
+
+        JButton threeButton = new JButton("3");
+        threeButton.setBounds(400,100,100,50);
+        this.add(threeButton);
+        threeButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "3";
+                numberButton(num);
+            }
+        });
+
+        JButton fourButton = new JButton("4");
+        fourButton.setBounds(400,100,100,50);
+        this.add(fourButton);
+        fourButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "4";
+                numberButton(num);
+            }
+        });
+
+        JButton fiveButton = new JButton("5");
+        fiveButton.setBounds(400,100,100,50);
+        this.add(fiveButton);
+        fiveButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "5";
+                numberButton(num);
+            }
+        });
+
+        JButton sixButton = new JButton("6");
+        sixButton.setBounds(400,100,100,50);
+        this.add(sixButton);
+        sixButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "6";
+                numberButton(num);
+            }
+        });
+
+        JButton sevenButton = new JButton("7");
+        sevenButton.setBounds(400,100,100,50);
+        this.add(sevenButton);
+        sevenButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "7";
+                numberButton(num);
+            }
+        });
+
+        JButton eightButton = new JButton("8");
+        eightButton.setBounds(400,100,100,50);
+        this.add(eightButton);
+        eightButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "8";
+                numberButton(num);
+            }
+        });
+
+        JButton nineButton = new JButton("9");
+        nineButton.setBounds(400,100,100,50);
+        this.add(nineButton);
+        nineButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                String num = "9";
+                numberButton(num);
+            }
+        });
+
+        JButton plusButton = new JButton("+");
+        plusButton.setBounds(400,100,100,50);
+        this.add(plusButton);
+        plusButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                setOperator("+");
+            }
+        });
+
+        JButton minusButton = new JButton("-");
+        minusButton.setBounds(400,100,100,50);
+        this.add(minusButton);
+        minusButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                setOperator("-");
+            }
+        });
+
+        JButton multButton = new JButton("*");
+        multButton.setBounds(400,100,100,50);
+        this.add(multButton);
+        multButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                setOperator("*");
+            }
+        });
+
+        JButton divButton = new JButton("/");
+        divButton.setBounds(400,100,100,50);
+        this.add(divButton);
+        divButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                setOperator("/");
+            }
+        });
+
+    }
+
+    private void enterButton(){
+        //do enter button stuff
+        int firstNum = 0;
+        int secondNum = 0;
+        int result = 0;
+        if(!firstArg.equals("")){
+            firstNum = Integer.parseInt(firstArg);
+        }
+        if(!secondArg.equals("")){
+            secondNum = Integer.parseInt(secondArg);
+        }
+
+        switch(operator){
+            case "+":
+                result =  firstNum + secondNum;
+                //LABEL STUFF
+                break;
+            case "-":
+                result =  firstNum - secondNum;
+                //LABEL STUFF
+                break;
+            case "*":
+                result =  firstNum * secondNum;
+                //LABEL STUFF
+                break;
+            case "/":
+                //do division
+                //remember div by 0
+                break;
+            default:
+                //set answer to 0?
+                break;
+        }
+
+    }
+
+    private void numberButton(String num){
+        //do stuff
+        //displayLabel.setText(num);
+        //update number1, if not operator
+        //if operator, then update num 2
+        //call UpdateView()
+
+    }
+
+    private void setOperator(String op){
+        this.operator = op;
+        //if clicked again, hit enter then set new?
+    }
+
+    public static void main(String[] args) {
+        CalculatorApp ex = new CalculatorApp();
+        ex.setVisible(true);
+    }
+}
+
