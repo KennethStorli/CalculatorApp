911f28878bcc8b91f5594459b784e3b050731e29
AmundF <amundfro@gmail.com>
2018-09-13 10:10:58 +0200

    feature(MathUtil): added subtraction and multiplication

--- src/MathUtil.java
+++ src/MathUtil.java
@@ -14,4 +14,10 @@ public class MathUtil {
 	public static int add(int number1,int number2){
 		return number1+number2;
 	}
+	public static int sub(int number1, int number2){
+		return number1-number2;
+	}
+	public static int mul(int number1, int number2){
+		return number1*number2;
+	}
 }
