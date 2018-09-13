cda9c80388b0fc2f1001305b5b335d8bf6a55001
AmundF <amundfro@gmail.com>
2018-09-13 10:17:31 +0200

    feat(MathUtil): added division

--- src/MathUtil.java
+++ src/MathUtil.java
@@ -20,4 +20,11 @@ public class MathUtil {
 	public static int mul(int number1, int number2){
 		return number1*number2;
 	}
+	public static int div(int number1, int number2){
+		if (number2 == 0){
+			throw new IllegalArgumentException("Division by zero");
+		}
+		return number1/number2;
+		
+	}
 }
