ae0c2ae96b624deefe9dac270b44a29110d775ef
AmundF <amundfro@gmail.com>
2018-09-13 10:22:55 +0200

    fix(MathUtil): cleanup

--- src/MathUtil.java
+++ src/MathUtil.java
@@ -1,16 +1,4 @@
 public class MathUtil {	
-	public static int doMath(int number1, int number2, char operator){
-		switch(operator){
-			case '*':	return number1*number2;
-			case '+':	return number1+number2;
-			case '-':	return number1-number2;
-			case '/':	if((number1 != 0)&&(number2 != 0)){
-				return number1/number2;
-			}else{return 0;}
-			default:	return 0;
-		}
-		
-	}
 	public static int add(int number1,int number2){
 		return number1+number2;
 	}
@@ -25,6 +13,5 @@ public class MathUtil {
 			throw new IllegalArgumentException("Division by zero");
 		}
 		return number1/number2;
-		
 	}
 }
